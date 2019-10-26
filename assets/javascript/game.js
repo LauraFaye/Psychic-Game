ac690673095fa9f60818b954200a3c7569ba9072
Laura Faye <jim.laura2@tds.net>
2019-10-26 12:06:04 -0400

    edit html and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,17 @@
-var computerChoices = ["r", "p", "s"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 var wins = 0;
 var losses = 0;
-var ties = 0;
+var numGuesses = 9;
+var guessChoices = [];
 
 var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text");
 var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var tiesText = document.getElementById("ties-text");
-
+var numGuesses = document.getElementById("numGuesses-text");
+var guessChoices = document.getElementById("guessChoices-text");
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
@@ -19,10 +20,10 @@ document.onkeyup = function (event) {
     var userChoice = event.key
     console.log(userChoice)
     wins = event.key
-example()
+    example()
 }
 
-function example(){
+function example() {
     console.log(wins)
 }
 console.log(wins)
